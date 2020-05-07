401c7cbbdbe86a48a30ac0f6b69c205367a9c752
Damian <sitarz.nomad@gmail.com>
2020-05-07 17:09:34 +0200

    Updates

---
+++ precache-manifest.af9f9ae9e6691f4a443606d63722ef40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9770420a180b02e325a690854ec75160",
+    "url": "/react-check-calendar/index.html"
+  },
+  {
+    "revision": "8afb1607432ed4f39546",
+    "url": "/react-check-calendar/static/css/main.660c9c86.chunk.css"
+  },
+  {
+    "revision": "1e3c242c4fb1a1c70ecb",
+    "url": "/react-check-calendar/static/js/2.3e7eaede.chunk.js"
+  },
+  {
+    "revision": "d72c695bb2fed22c8cbf125cbcb911ef",
+    "url": "/react-check-calendar/static/js/2.3e7eaede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afb1607432ed4f39546",
+    "url": "/react-check-calendar/static/js/main.f762b2dc.chunk.js"
+  },
+  {
+    "revision": "b345f9f88ce54cff7468",
+    "url": "/react-check-calendar/static/js/runtime-main.7de6d5dc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-check-calendar/precache-manifest.af9f9ae9e6691f4a443606d63722ef40.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-check-calendar/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
