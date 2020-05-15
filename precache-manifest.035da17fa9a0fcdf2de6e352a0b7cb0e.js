c4652b390f2cf022377fe3bf5958a92ca473ce49
dsitarz <dsitarz@najlepszefoto.pl>
2020-05-15 14:00:53 +0200

    Updates

---
+++ precache-manifest.035da17fa9a0fcdf2de6e352a0b7cb0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f14183243f3c444a99ca3ab1877bd2",
+    "url": "/react-check-calendar/index.html"
+  },
+  {
+    "revision": "93443f27569a55b70083",
+    "url": "/react-check-calendar/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "6124eede03718e456152",
+    "url": "/react-check-calendar/static/css/main.5175059f.chunk.css"
+  },
+  {
+    "revision": "93443f27569a55b70083",
+    "url": "/react-check-calendar/static/js/2.9360387b.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/react-check-calendar/static/js/2.9360387b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6124eede03718e456152",
+    "url": "/react-check-calendar/static/js/main.4115a46e.chunk.js"
+  },
+  {
+    "revision": "05695c592654faca20de",
+    "url": "/react-check-calendar/static/js/runtime-main.c10de9c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-check-calendar/precache-manifest.7f53793fae21a38aee3e06980e714076.js"
+  "/react-check-calendar/precache-manifest.035da17fa9a0fcdf2de6e352a0b7cb0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-check-calendar/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
